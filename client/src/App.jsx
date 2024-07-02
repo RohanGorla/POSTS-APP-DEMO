98cca47d0b417dc2551945c3c68793bd0de988f2
RohanGorla <rohangorla2010@gmail.com>
2024-07-03 04:17:40 +0530

    integrate MySQL DB and render images by getting pre signed urls

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,9 @@ import "./App.css";
 
 function App() {
   const [file, setfile] = useState();
-  const [img, setImg] = useState();
+  const [cap, setCap] = useState("");
+  const [key, setKey] = useState("");
+  const [data, setData] = useState();
   const [render, setRender] = useState(false);
 
   async function handleSubmit(e) {
@@ -24,21 +26,30 @@ function App() {
     // formData.append();
   }
 
+  async function getUrl(key) {
+    let res = await axios.post("http://localhost:3000/url", {
+      key: key,
+    });
+    console.log(`Signed url for ${key} ->`, res.data);
+    return res.data;
+  }
+
   async function assignURL(e) {
     e.preventDefault();
-    console.log(file.type);
+    // console.log(file.type);
     let res = await axios.post("http://localhost:3000/assign", {
-      key: "tengen-uzui",
+      key: key,
       type: file.type,
+      caption: cap,
     });
-    console.log(res.data);
+    // console.log(res.data);
     let res2 = await axios.put(res.data, file, {
       headers: {
         "Content-Type": file.type,
       },
     });
     // let res2 = await axios.put(res.data, file);
-    console.log(res2);
+    // console.log(res2);
     setRender(!render);
   }
 
@@ -47,29 +58,80 @@ function App() {
   // }, [file]);
 
   useEffect(() => {
-    async function getUrl() {
+    async function getData() {
       let res = await axios.get("http://localhost:3000/");
-      console.log(res.data);
-      setImg(res.data);
+      // console.log(res);
+      // let Posts = [];
+      // res.data.forEach(async (post) => {
+      //   let url = await getUrl(post.imageTag);
+      //   post.url = url;
+      //   Posts.push(post);
+      //   setData(Posts);
+      // });
+      setData(res.data);
     }
-    getUrl();
+    getData();
   }, [render]);
 
   return (
     <>
       <h1>Multer - AWS S3 test!</h1>
       <form onSubmit={assignURL}>
-        <input
-          type="file"
-          accept="image/*"
-          required
-          onChange={(e) => {
-            setfile(e.target.files[0]);
-          }}
-        ></input>
+        <div style={{ margin: "1em 0" }}>
+          <input
+            type="file"
+            accept="image/*"
+            required
+            onChange={(e) => {
+              setfile(e.target.files[0]);
+            }}
+          ></input>
+          <input
+            type="text"
+            value={key}
+            onChange={(e) => {
+              setKey(e.target.value);
+            }}
+            placeholder="Enter name of the image"
+          ></input>
+        </div>
+        <div>
+          <textarea
+            cols="50"
+            rows="3"
+            onChange={(e) => {
+              setCap(e.target.value);
+            }}
+            value={cap}
+            placeholder="Enter caption here..."
+          ></textarea>
+        </div>
         <input type="submit"></input>
       </form>
-      {img && <img style={{ width: "500px" }} src={img}></img>}
+      {data && (
+        <div>
+          {data.map((post) => {
+            console.log(post);
+            return (
+              <div
+                key={post.id}
+                style={{
+                  margin: "4em 0",
+                  border: "solid white 2px",
+                  padding: "1em",
+                }}
+              >
+                <div>
+                  <img style={{ width: "500px" }} src={post.url}></img>
+                </div>
+                <div>
+                  <h2>{post.caption}</h2>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </>
   );
 }
--- server/server.js
+++ server/server.js
@@ -20,6 +20,19 @@ const bucketRegion = process.env.BUCKET_REGION;
 const accessKey = process.env.ACCESS_KEY;
 const secretKey = process.env.SECRET_KEY;
 
+const db = mysql.createConnection({
+  database: process.env.DATABASE,
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+db.connect((err) => {
+  if (err) console.log(err);
+  else console.log("Database connected");
+});
+
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
@@ -32,12 +45,31 @@ const s3 = new S3Client({
 });
 
 app.get("/", async (req, res) => {
+  db.query("select * from postInfo", async (err, data) => {
+    if (err) return res.send(err);
+    for (const post of data) {
+      const getObjectParams = {
+        Bucket: bucketName,
+        Key: post.imageTag,
+      };
+      const command = new GetObjectCommand(getObjectParams);
+      const url = await getSignedUrl(s3, command, { expiresIn: 60 });
+      post.url = url;
+    }
+    console.log(data);
+    res.send(data);
+  });
+});
+
+app.post("/url", async (req, res) => {
   const getObjectParams = {
     Bucket: bucketName,
-    Key: "tengen-uzui.png",
+    Key: req.body.key,
   };
   const command = new GetObjectCommand(getObjectParams);
-  const url = await getSignedUrl(s3, command, { expiresIn: 10 });
+  // const data = await s3.send(command);
+  // console.log("data is ->", data);
+  const url = await getSignedUrl(s3, command, { expiresIn: 60 });
   console.log(url);
   res.json(url);
 });
@@ -59,16 +91,32 @@ app.post("/send", upload.single("photo"), async (req, res) => {
 
 app.post("/assign", async (req, res) => {
   console.log(req.body);
-  const key = req.body.key;
+  const key = `${req.body.key}-${Date.now()}`;
   const fileType = req.body.type;
+  const caption = req.body.caption;
+  const extension = fileType.split("/")[1];
+  const fullKey = key + "." + extension;
+  console.log(extension);
   const params = {
     Bucket: bucketName,
-    Key: key+'.png',
+    Key: fullKey,
     ContentType: fileType,
   };
   console.log("ContentType is ", params.ContentType);
   const command = new PutObjectCommand(params);
   const url = await getSignedUrl(s3, command);
+  let values = [caption, fullKey];
+  db.query(
+    "insert into postInfo (caption, imageTag) values (?)",
+    [values],
+    (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.json(err);
+      }
+      console.log(data);
+    }
+  );
   res.json(url);
 });
 
